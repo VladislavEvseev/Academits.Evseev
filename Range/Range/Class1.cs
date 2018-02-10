1a82a0c17cc686c77892883dd364ccd6f50b9dbe
Владислав Евсеев <vladislav.ev@gmail.com>
2018-02-10 18:48:00 +0700

    Исправленная версия

---
+++ Range/Range/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Class1
+    {
+        private double from;
+        private double to;
+
+        public Class1 (double from, double to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public double GetLength()
+        {
+            return to - from;
+        }
+
+        public bool IsInside(double numberInRange)
+        {
+            return from <= numberInRange && to >= numberInRange;
+        }
+
+    }
+}
---
+++ Range/Range/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 r = new Class1(2, 22);
+
+            Console.WriteLine("Length=" + r.GetLength());
+
+            double numberInRange = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(r.IsInside(numberInRange));
+            Console.ReadKey();
+        }
+
+    }
+}
