be360e534bcfce2810e459880c13ee7c78729ba1
Владислав Евсеев <vladislav.ev@gmail.com>
2018-04-11 17:52:56 +0700

    Переработал

---
+++ Shapes/Shapes/ClassShape/Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Square : IShape
+    {
+        private double width;
+
+        public Square(double width)
+        {
+            this.width = width;
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+
+        public double GetHeight()
+        {
+            return width;
+        }
+
+        public double GetArea()
+        {
+            return Math.Pow(width, 2);
+        }
+
+        public double GetPerimeter()
+        {
+            return (width * 4);
+        }
+    }
+}
---
+++ Shapes/Shapes/ClassShape/Triangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Triangle : IShape
+    {
+        private double x1;
+        private double y1;
+        private double x2;
+        private double y2;
+        private double x3;
+        private double y3;
+
+        public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+        }
+
+        public double GetWidth()
+        {
+            return Math.Max(Math.Max(x3, x1), x2) - Math.Min(Math.Min(x3, x1), x2);
+        }
+
+        public double GetHeight()
+        {
+            return Math.Max(Math.Max(y3, y1), y2) - Math.Min(Math.Min(y3, y1), y2);
+        }
+
+        public double GetArea()
+        {
+            return 0.5 * ((x1 - x3) * (y2 - y3) + (x2 - x3) * (y1 - y3));
+        }
+
+        private static double GetLengthOfSideOfTriangle(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+
+        public double GetPerimeter()
+        {
+            return GetLengthOfSideOfTriangle(x1, y1, x2, y2) + GetLengthOfSideOfTriangle(x2, y2, x3, y3) + GetLengthOfSideOfTriangle(x1, y1, x3, y3);
+        }
+    }
+}
